ae820422c2ccb72e6d975bdd51477eac959dbead
dustywin <125769813+dustywin@users.noreply.github.com>
2023-11-10 13:33:51 -0600

    kawaii

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentric.java
@@ -57,7 +57,7 @@ public class FieldCentric extends OpMode {
         bot.setSlowDownState(slowDownState.FULL);
     }
 
-    // ---------------------------- LOOPING ---------------------------- //
+        // ---------------------------- LOOPING ---------------------------- //
 
     @Override
     public void loop()
