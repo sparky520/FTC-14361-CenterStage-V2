f6c94ec7ab3095e33eb3b0b6dc1d684eafeed7e3
AravNeroth <reacharav@outlook.com>
2023-11-09 21:41:32 -0600

    made an auton folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomouse/LeftBlueHardcodePark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomouse/LeftBlueHardcodePark.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
+package org.firstinspires.ftc.teamcode.Autonomouse;
 
 import com.qualcomm.hardware.lynx.LynxVoltageSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
