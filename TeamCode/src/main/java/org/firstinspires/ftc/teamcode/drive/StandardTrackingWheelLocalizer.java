92c668a9b2346d28e4f1c34949c1d27c12615108
dustywin <125769813+dustywin@users.noreply.github.com>
2023-11-17 23:49:48 -0600

    balls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -34,8 +34,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double LATERAL_DISTANCE = 10.192056985033322; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = -5.25; // in; offset of the lateral wheel
 
-    public static double X_MULTIPLIER = 0; // Multiplier in the X direction
-    public static double Y_MULTIPLIER = 0; // Multiplier in the Y
+    public static double X_MULTIPLIER = 1; // Multiplier in the X direction
+    public static double Y_MULTIPLIER = 1; // Multiplier in the Y
     // direction
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
