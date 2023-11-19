51494407fa06ca51674630d632c9b9b25868405b
dustywin <125769813+dustywin@users.noreply.github.com>
2023-11-18 18:51:21 -0600

    PID Values Fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -57,9 +57,13 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.00151;
-    public static double kA = 0.016116258833585406;
+    public static double kV = 0.016116258833585406;
+    public static double kA = 0.00151;
     public static double kStatic = 0;
+
+    // * THESE ARE THE PID VALUES*
+    //kA = 0.00151
+    //kV = 0.016116258833585406
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -90,9 +94,6 @@ public class DriveConstants {
 
      */
 
-    //
-    //kA = 0.00151
-    //kV = 0.016116258833585406
     public static double MAX_VEL = 62.07043729;
     public static double MAX_ACCEL = 30;
     public static double MAX_ANG_VEL = Math.toRadians(360);
