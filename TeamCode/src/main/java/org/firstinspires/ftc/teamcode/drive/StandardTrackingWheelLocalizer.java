94a22d01160a37df5a1693705ecbc73e4c5cf5dc
gulin225 <ggulin225@gmail.com>
2023-11-25 15:07:08 -0600

    tuning fisnished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,7 +31,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.6889764; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 10.158668004258784; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 9.18; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = -5.25; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 1.02397; // Multiplier in the X direction
@@ -58,6 +58,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
 
         rightEncoder.setDirection(Encoder.Direction.REVERSE);
         leftEncoder.setDirection(Encoder.Direction.REVERSE);
+        frontEncoder.setDirection(Encoder.Direction.REVERSE);
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
     }
 
