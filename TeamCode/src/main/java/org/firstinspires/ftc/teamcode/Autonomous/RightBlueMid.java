56c79e90731fb85050ad679c6021d4e9ec3caea9
dustywin <125769813+dustywin@users.noreply.github.com>
2023-11-25 00:23:59 -0600

    iui

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RightBlueMid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RightBlueMid.java
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 public class RightBlueMid extends LinearOpMode
 {
     public Robot bot;
-    Pose2d myPose = new Pose2d(63, -36, Math.toRadians(90));
+    Pose2d myPose = new Pose2d(-36, 63, Math.toRadians(90));
     @Override
     public void runOpMode()
     {
@@ -24,52 +24,43 @@ public class RightBlueMid extends LinearOpMode
         drive.setPoseEstimate(myPose);
 
         Trajectory Traj1 = drive.trajectoryBuilder(myPose)
-                .addTemporalMarker(0.2, () -> {
-                    bot.setClawPosition(clawState.close);
-                    bot.setClawState(clawState.close);
-                    bot.setWristPosition(wristState.normal);
-                    bot.setWristState(wristState.normal);
-                    bot.setVirtualFourBarPosition(virtualFourBarState.init, virtualFourBarExtensionState.extending);
-                    bot.setVirtualFourBarState(virtualFourBarState.init);
-                })
-                .forward(30)
+//                .addTemporalMarker(0.2, () -> {
+//                    bot.setClawPosition(clawState.close);
+//                    bot.setClawState(clawState.close);
+//                    bot.setWristPosition(wristState.normal);
+//                    bot.setWristState(wristState.normal);
+//                    bot.setVirtualFourBarPosition(virtualFourBarState.init, virtualFourBarExtensionState.extending);
+//                    bot.setVirtualFourBarState(virtualFourBarState.init);
+//                })
+                .back(19)
                 .build();
 
         Trajectory Traj2 = drive.trajectoryBuilder(Traj1.end())
-                .back(6)
+                .forward(6)
                 .build();
 
         Trajectory Traj3 = drive.trajectoryBuilder(Traj2.end())
-                .strafeRight(20)
+                .strafeLeft(20)
                 .build();
 
         Trajectory Traj4 = drive.trajectoryBuilder(Traj3.end())
-                .forward(24)
+                .back(18)
                 .build();
 
         Trajectory Traj5 = drive.trajectoryBuilder(Traj4.end())
-                .lineToLinearHeading(new Pose2d(-12, 12, Math.toRadians(180)))
-                .addTemporalMarker(0.2, () -> {
-                    bot.setWristPosition(wristState.sideways);
-                    bot.setWristState(wristState.sideways);
-                    bot.setVirtualFourBarPosition(virtualFourBarState.outtaking, virtualFourBarExtensionState.extending);
-                    bot.setVirtualFourBarState(virtualFourBarState.outtaking);
-                })
+                .strafeRight(90)
                 .build();
 
         Trajectory Traj6 = drive.trajectoryBuilder(Traj5.end())
-                .splineToLinearHeading(new Pose2d(-36,48),Math.toRadians(180))
-                .addTemporalMarker(0.2, () -> {
-                bot.setClawPosition(clawState.open);
-                })
+                .lineToLinearHeading(new Pose2d(48, 36, Math.toRadians(180)))
                 .build();
 
         Trajectory Traj7 = drive.trajectoryBuilder(Traj6.end())
-                .strafeRight(10)
+                .strafeLeft(10)
                 .build();
 
         Trajectory Traj8 = drive.trajectoryBuilder(Traj6.end())
-                .forward(10)
+                .back(10)
                 .build();
 
         waitForStart();
