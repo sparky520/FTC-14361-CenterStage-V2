2f3dcd74df602572a444cbda736f4ce777cfb268
AravNeroth <reacharav@outlook.com>
2023-11-28 17:00:34 -0600

    added camera to all classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RightBluePark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RightBluePark.java
@@ -5,16 +5,27 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Subsystems.HSVBlueDetection;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous(name = "RightBluePark")
 public class RightBluePark extends LinearOpMode
 {
+    OpenCvCamera camera;
+    HSVBlueDetection blueDetection;
+    String webcamName;
     Pose2d myPose = new Pose2d(-63, -36, Math.toRadians(180));
     @Override
     public void runOpMode()
     {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        initCam();
+
 
         drive.setPoseEstimate(myPose);
 
@@ -30,6 +41,23 @@ public class RightBluePark extends LinearOpMode
 
         waitForStart();
 
+        /* to get detection
+        switch (blueDetection.getLocation()) {
+            case LEFT:
+                // ...
+                break;
+            case RIGHT:
+                // ...
+                break;
+            case MIDDLE:
+                // ...
+        }
+         */
+
+        // to save battery
+        camera.stopStreaming();
+
+
         if(isStopRequested()) return;
 
         drive.followTrajectory(Traj1);
@@ -37,5 +65,45 @@ public class RightBluePark extends LinearOpMode
 
         drive.turn(-1);
         drive.followTrajectory(Traj3);
+    }
+
+    private void initCam() {
+
+        //This line retrieves the resource identifier for the camera monitor view. The camera monitor view is typically used to display the camera feed
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        webcamName = "Webcam 1";
+
+        // This line creates a webcam instance using the OpenCvCameraFactor with the webcam name (webcamName) and the camera monitor view ID.
+        // The camera instance is stored in the camera variable that we can use later
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
+
+        // initializing our Detection class (details on how it works at the top)
+        blueDetection = new HSVBlueDetection(telemetry);
+
+        // yeah what this does is it gets the thing which uses the thing so we can get the thing
+        /*
+        (fr tho idk what pipeline does, but from what I gathered,
+         we basically passthrough our detection into the camera
+         and we feed the streaming camera frames into our Detection algorithm)
+         */
+        camera.setPipeline(blueDetection);
+
+        /*
+        this starts the camera streaming, with 2 possible combinations
+        it starts streaming at a chosen res, or if something goes wrong it throws an error
+         */
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.showFpsMeterOnViewport(true);
+                camera.startStreaming(320, 240, OpenCvCameraRotation.SENSOR_NATIVE);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addLine("Unspecified Error Occurred; Camera Opening");
+            }
+        });
     }
 }
