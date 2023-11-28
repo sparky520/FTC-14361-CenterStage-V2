b30036118ddf77baaad53d8385b7891134a655df
AravNeroth <reacharav@outlook.com>
2023-11-28 17:24:43 -0600

    blue hsv detection imported

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/BlueDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/BlueDetection.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -6,144 +8,131 @@ import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
-public class BlueDetection extends OpenCvPipeline {
 
-    public enum TSEPosition {
+/*
+******* NOTE *******
+This detection is used for the FTC 2023-2024 CENTERSTAGE Challenge by the CPHS Robolobos (14361)
+
+This code is heavily inspired and guided by team Wolf Corp Robotics 12525 (I love these people)
+
+here's the vid that step by step saved my sanity & explains how this version of HSV color detection works:
+https://www.youtube.com/watch?v=JO7dqzJi8lw&ab_channel=FTCteamWolfCorpRobotics12525
+
+I've added some comments abt stuff- Good luck!
+ */
+public class BlueDetection extends OpenCvPipeline {
+    Telemetry telemetry;
+    Mat mat = new Mat();
+    public enum Location {
         LEFT,
-        MIDDLE,
-        RIGHT
+        RIGHT,
+        MIDDLE
     }
 
-    // TOPLEFT anchor point for the bounding box
-    private static Point LEFTBOX_TOPLEFT_ANCHOR_POINT = new Point(50, 135); //I think this is the actual one we edit! Increase x goes right, increase y goes down.
+    // lowkey i barely know what volatiles is- just google it
+    private volatile Location location = Location.RIGHT;
 
-    private static Point MIDDLEBOX_TOPLEFT_ANCHOR_POINT = new Point(200, 155); //I think this is the actual one we edit! Increase x goes right, increase y goes down.
+    /*
+    These create the rectangles that your TSE should be in.
+    See what the camera does by using the camera str    eam in the menu of the driver station WHILE the bot is active
+    Adjust the camera or the boxes so your TSE is inside it
 
-    // Width and height for the bounding box
-    public static int REGION_WIDTH = 40;
-    public static int REGION_HEIGHT = 40;
+     */
+    static final Rect LEFT_ROI = new Rect(
+            new Point(45, 40),
+            new Point(90, 90));
+    static final Rect MIDDLE_ROI = new Rect(
+            new Point(215, 40),
+            new Point(255, 90));
+    static double PERCENT_COLOR_THRESHOLD = 0.4;
 
-    // Color definitions
-    private final Scalar WHITE = new Scalar(255, 255, 255);
+    public BlueDetection(Telemetry t) { telemetry = t; }
 
-    private final Scalar BLUE = new Scalar(0, 0, 255);
-
-    // Anchor point definitions
-    Point LEFT_left_pointA = new Point(
-            LEFTBOX_TOPLEFT_ANCHOR_POINT.x,
-            LEFTBOX_TOPLEFT_ANCHOR_POINT.y);
-    Point LEFT_left_pointB = new Point(
-            LEFTBOX_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-            LEFTBOX_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-
-    Point MIDDLE_left_pointA = new Point(
-            MIDDLEBOX_TOPLEFT_ANCHOR_POINT.x,
-            MIDDLEBOX_TOPLEFT_ANCHOR_POINT.y);
-    Point MIDDLE_left_pointB = new Point(
-            MIDDLEBOX_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-            MIDDLEBOX_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-
-    // Running variable storing the parking position
-    private volatile TSEPosition position = TSEPosition.RIGHT;
-
-    Scalar leftSumColors, middleSumColors;
-
-    private double blueThreshold;
-    // this can be called to show how much
     @Override
     public Mat processFrame(Mat input) {
-        // Get the submat frame, and then sum all the values
-        Mat leftAreaMat = input.submat(new Rect(LEFT_left_pointA, LEFT_left_pointB));
-        leftSumColors = Core.sumElems(leftAreaMat);
 
-        Mat middleAreaMat = input.submat(new Rect(MIDDLE_left_pointA, MIDDLE_left_pointB));
-        middleSumColors = Core.sumElems(middleAreaMat);
+        // changes the frame captured by the camera from RGB to HSV
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV); // Imgproc.COLOR_RGB2HSV & Imgproc.COLOR_RGB2HSV_FULL are the same; FULL is the scale 0-360 & normal is 0-180
 
-        // Threshold for blue color detection
         /*
-        LOWERING THIS VALUE WILL LOWER THE THRESHOLD NEEDED TO DETECT THE COLOR BLUE
+        HSV = [ HUE (color), SATURATION (grey), VALUE (brightness) ]
+        HSV has a low end & high end of the spectrum, and here we set the range of color we want
 
-        too high, it will need a LOT of blue
-        too low, it will say everything is blue
-
-        to tune, increase/decrease it by 10 till you are satisfied
-
-        lighting can and will effect this,
+        how do i know what values to put in ???
+        use this handy dandy link i found (please do not click on any links/ads, it is super sketchy)
+        https://www.tydac.ch/color/
          */
-        blueThreshold = 150;
 
-        // Check if the blue color is present in the left rectangle
-        // val[] is an ArrayList, and the way the class passes the values of R G B,
-        //
-        if (leftSumColors.val[0] > blueThreshold) {
-            position = TSEPosition.LEFT;
-            Imgproc.rectangle(
-                    input,
-                    LEFT_left_pointA,
-                    LEFT_left_pointB,
-                    BLUE,
-                    3
-            );
+        // *** don't forget to divide the values by 2 if you use Imgproc.COLOR_RBG2HSV
+
+        // in this case, we using dark blue to light blue
+        Scalar lowHSV = new Scalar(100, 50, 72);
+        Scalar highHSV = new Scalar(127, 255, 242);
+
+
+        // this shows us the stuff in our range (in this case blue)
+        // mat is the matrix, and we define our
+        Core.inRange(mat, lowHSV, highHSV, mat);
+
+        Mat left = mat.submat(LEFT_ROI);
+        Mat middle = mat.submat(MIDDLE_ROI);
+
+        double leftValue = Core.sumElems(left).val[0] / LEFT_ROI.area() / 255;
+        double middleValue = Core.sumElems(middle).val[0] / MIDDLE_ROI.area() / 255;
+
+        left.release();
+        middle.release();
+
+        telemetry.addData("Left raw value", (int) Core.sumElems(left).val[0]);
+        telemetry.addData("Middle raw value", (int) Core.sumElems(middle).val[0]);
+        telemetry.addData("Left percentage", Math.round(leftValue * 100) + "%");
+        telemetry.addData("Middle percentage", Math.round(middleValue * 100) + "%");
+
+        boolean tseLeft = leftValue > PERCENT_COLOR_THRESHOLD;
+        boolean tseMiddle = middleValue > PERCENT_COLOR_THRESHOLD;
+
+
+        /*
+        THIS IS DIFFERENT FROM THE VIDEO!!
+        in the video, the detection system is made to find normal stones because they're yellow
+        and easy to detect, and depending on where the normal stones are, it knows where the skystone is
+
+        this altered version I've made is for Centerstage, and our TSE colors are either red/blue;
+        Which means it's much simpler for us that we only need to detect the color red/blue
+
+        That's why his code is 'reversed'- he's not actually detecting the Skystone- just the normal ones
+         */
+        if (tseLeft) {
+            location = Location.LEFT;
+            telemetry.addData("TSE Location: ", "LEFT");
         }
-
+        else if(tseMiddle) {
+            location = Location.MIDDLE;
+            telemetry.addData("TSE Location: ", "MIDDLE");
+        }
         else {
-            Imgproc.rectangle(
-                    input,
-                    LEFT_left_pointA,
-                    LEFT_left_pointB,
-                    WHITE,
-                    3
-            );
+            location = Location.RIGHT;
+            telemetry.addData("TSE not detected; Location: ", "RIGHT");
         }
 
-        // Check if the blue color is present in the middle rectangle
-        if (middleSumColors.val[0] > blueThreshold) {
-            position = TSEPosition.MIDDLE;
-            Imgproc.rectangle(
-                    input,
-                    MIDDLE_left_pointA,
-                    MIDDLE_left_pointB,
-                    BLUE,
-                    3
-            );
-        }
-
-        else {
-            // If blue is not detected in spot 2, set the outline to white
-            Imgproc.rectangle(
-                    input,
-                    MIDDLE_left_pointA,
-                    MIDDLE_left_pointB,
-                    WHITE,
-                    3
-            );
-        }
+        telemetry.update();
 
 
-        // Release Mat objects
-        leftAreaMat.release();
-        middleAreaMat.release();
+        // change the img back to RGB
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB);
 
-        // Return the modified input Mat
-        return input;
+        // below will make the boxes Red if no TSE exists, and Green if it does within the rectangle box
+        Scalar noTSE = new Scalar(255, 0, 0);
+        Scalar tseDetected = new Scalar(0, 255, 0);
+
+        // depending on where the TSEe is, or where it isn't, the color of the rectangle will change
+        Imgproc.rectangle(mat, LEFT_ROI, location == Location.LEFT? tseDetected:noTSE);
+        Imgproc.rectangle(mat, MIDDLE_ROI, location == Location.MIDDLE? tseDetected:noTSE);
+
+        return mat;
     }
 
-
-    // Returns an enum being the current position where the robot will park
-    public TSEPosition getPosition() {
-        return position;
+    public Location getLocation() {
+        return location;
     }
-
-    public double getLeftBoxBlueReading(){
-        return leftSumColors.val[0];
-    }
-
-    public double getMiddleBoxBlueReading(){
-        return middleSumColors.val[0];
-    }
-
-    public double getBlueThreshold(){
-        return blueThreshold;
-    }
-
 }
